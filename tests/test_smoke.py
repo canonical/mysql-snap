262a1060f4f58f8584e250ad908a4f1fa964c479
Paulo Machado <paulo.machado@canonical.com>
2025-09-22 22:21:43 -0300

    remove install from smoke.py

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -4,28 +4,9 @@
 import subprocess
 import time
 
-import pytest
 import yaml
 
 
-def test_install():
-    with open("snap/snapcraft.yaml") as file:
-        snapcraft = yaml.safe_load(file)
-
-    snap_name = snapcraft['name']
-    snap_version = snapcraft['version']
-
-    subprocess.run(
-        f"sudo snap remove --purge {snap_name}".split(),
-        check=True,
-    )
-    subprocess.run(
-        f"sudo snap install ./{snap_name}_{snap_version}_amd64.snap --dangerous".split(),
-        check=True,
-    )
-
-
-@pytest.mark.run(after="test_install")
 def test_all_apps():
     with open("snap/snapcraft.yaml") as file:
         snapcraft = yaml.safe_load(file)
@@ -34,7 +15,7 @@ def test_all_apps():
     skip = []
 
     snap_apps = snapcraft["apps"]
-    snap_name = snapcraft['name']
+    snap_name = snapcraft["name"]
 
     for app, data in snap_apps.items():
         if bool(data.get("daemon")) or app in skip:
@@ -51,7 +32,6 @@ def test_all_apps():
         )
 
 
-@pytest.mark.run(after="test_install")
 def test_all_services():
     with open("snap/snapcraft.yaml") as file:
         snapcraft = yaml.safe_load(file)
@@ -59,23 +39,17 @@ def test_all_services():
     skip = []
 
     snap_apps = snapcraft["apps"]
-    snap_name = snapcraft['name']
+    snap_name = snapcraft["name"]
 
     for app, data in snap_apps.items():
         if not bool(data.get("daemon")) or app in skip:
             continue
 
         print(f"Running {snap_name}.{app}...")
-        subprocess.check_output(
-            f"sudo snap start {snap_name}.{app}".split()
-        )
+        subprocess.check_output(f"sudo snap start {snap_name}.{app}".split())
 
         time.sleep(5)
-        service = subprocess.check_output(
-            f"snap services {snap_name}.{app}".split()
-        )
-        subprocess.check_output(
-            f"sudo snap stop {snap_name}.{app}".split()
-        )
+        service = subprocess.check_output(f"snap services {snap_name}.{app}".split())
+        subprocess.check_output(f"sudo snap stop {snap_name}.{app}".split())
 
         assert "active" in service.decode()
